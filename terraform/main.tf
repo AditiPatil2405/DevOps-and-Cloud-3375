9e1bced210bcd624bdb38567a12d9310a28f895c
preslysolomon <preslysolomon@gmail.com>
2024-03-12 09:40:37 -0400

    l

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,64 +2,11 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-locals {
-  eks_cluster_name = "my_cluster"
-}
+resource "aws_s3_bucket" "S3" {
+  bucket = "my-test3-bucket"
 
-
-
-# Create EKS Cluster
-module "eks_cluster" {
-  source = "terraform-aws-modules/eks/aws"
-
-  cluster_name    = local.eks_cluster_name
-  subnets         = ["subnet-xxxxxxxxxxxxxx", "subnet-yyyyyyyyyyyyyy"]# Update with your subnet IDs
-  vpc_id          = "vpc-08a5775fdb38b9a62" # Update with your VPC ID
-  node_groups     = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 2
-      min_capacity     = 1
-    }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-}
-
-# Execute AWS CLI command to get EKS cluster credentials
-resource "null_resource" "get_eks_credentials" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      aws eks --region your_aws_region update-kubeconfig --name ${local.eks_cluster_name}
-    EOT
-  }
-
-  depends_on = [
-    module.eks_cluster,
-    module.enable_aws_apis
-  ]
-}
-
-# Apply Kubernetes manifest configurations
-resource "null_resource" "apply_deployment" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "kubectl apply -f ${path.module}/your_kubernetes_manifests_directory -n your_namespace"
-  }
-
-  depends_on = [
-    null_resource.get_eks_credentials
-  ]
-}
-
-# Wait condition for all Pods to be ready before finishing
-resource "null_resource" "wait_conditions" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = <<-EOT
-      kubectl wait --for=condition=READY pods --all -n your_namespace --timeout=280s
-    EOT
-  }
-
-  depends_on = [
-    null_resource.apply_deployment
-  ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,65 +1,47 @@
-variable "aws_region" {
-  type        = string
-  description = "The AWS region to apply this config to"
+variable "cluster_name" {
+  type = string
+  default = "my-eks-cluster"
 }
 
-variable "eks_cluster_name" {
-  type        = string
-  description = "Name given to the new EKS cluster"
-  default     = "online-boutique"
+variable "cluster_version" {
+  type = number
+  default = 1.25
 }
 
-variable "eks_cluster_version" {
-  type        = string
-  description = "The Kubernetes version to use for the EKS cluster"
-  default     = "1.21"  # Choose the desired version
+variable "region" {
+  type = string
+  default = "ca-central-1"
 }
 
-variable "eks_cluster_node_group_name" {
-  type        = string
-  description = "Name given to the EKS node group"
-  default     = "eks-nodes"
+variable "availability_zones" {
+  type = list
+  default = ["ca-central-1a", "ca-central-1b"]
 }
 
-variable "eks_cluster_node_group_desired_capacity" {
-  type        = number
-  description = "Desired number of nodes in the EKS node group"
-  default     = 2
-}
 
-variable "eks_cluster_node_group_max_capacity" {
-  type        = number
-  description = "Maximum number of nodes in the EKS node group"
-  default     = 2
-}
 
-variable "eks_cluster_node_group_min_capacity" {
-  type        = number
-  description = "Minimum number of nodes in the EKS node group"
-  default     = 1
-}
+variable "addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
 
-variable "eks_cluster_subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs where the EKS cluster will be deployed"
-  # Update with your subnet IDs
-  default     = ["subnet-xxxxxxxxxxxxxx", "subnet-yyyyyyyyyyyyyy", "subnet-zzzzzzzzzzzzzz"]
-}
-
-variable "eks_cluster_vpc_id" {
-  type        = string
-  description = "VPC ID where the EKS cluster will be deployed"
-  # Update with your VPC ID
-  default     = "vpc-xxxxxxxxxxxxxxxx"
-}
-
-variable "kubernetes_manifests_directory" {
-  type        = string
-  description = "Path to Online Boutique's Kubernetes resources, written using Kustomize"
-  default     = "../kustomize/"
-}
-
-variable "enable_memorystore" {
-  type        = bool
-  description = "If true, Online Boutique's in-cluster Redis cache will be replaced with an AWS ElastiCache Redis cache"
+  default = [
+    {
+      name    = "kube-proxy"
+      version = "v1.25.6-eksbuild.1"
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.12.2-eksbuild.1"
+    },
+    {
+      name    = "coredns"
+      version = "v1.9.3-eksbuild.2"
+    },
+    {
+      name    = "aws-ebs-csi-driver"
+      version = "v1.23.0-eksbuild.1"
+    }
+  ]
 }
