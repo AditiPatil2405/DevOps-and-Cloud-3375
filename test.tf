c8beb1120a4aac4c843a9ee784fae218138c40cb
Aditi Ravindra Patil <aditipcad@gmail.com>
2024-03-24 20:47:00 -0400

    Update TF

--- test.tf
+++ test.tf
@@ -1,25 +1,16 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "main"
-  cidr = "10.0.0.0/16"
+  name = "single-instance"
 
-  azs             = var.availability_zones
-  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
-  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 }
